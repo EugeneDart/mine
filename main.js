cd1b4455a9c3527485e3acc706a047d2547b889f
EugeneDart <90830214+EugeneDart@users.noreply.github.com>
2021-10-23 00:30:15 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+/* Мой город */
+let cityname = 'Винница';
+    console.log('cityname');
+let countryname = 'Украина';
+    console.log('countryname');
+let populationnumb = 350.000;
+    console.log('populationnumb');
+
+let stadiumyes = true;
+let stadiumno = false;
+
+    if(stadiumyes == true) {
+        console.log('Да, у нас есть футбольный стадион');
+    } else { 
+        console.log('Нет, у нас есть футбольный стадион');
+    }
